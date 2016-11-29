bd3ff0a8e9c4a0dc785b09870df324b616fc2623
Andrew90 <drewan@rambler.ru>
2016-11-29 14:52:15 +0500

    CommunicationIDItems почистил

--- AddTresholdWindowProj/SolidGroupAlgoritm/CommunicationIDItems.cpp
+++ AddTresholdWindowProj/SolidGroupAlgoritm/CommunicationIDItems.cpp
@@ -11,89 +11,27 @@ void CommunicationIDItems::LoadItem(wchar_t *name, int ind)
 	items.push_back(p);
 }
 
-void CommunicationIDItems::Set(wchar_t *name, int ind)
-{
-	for(auto k = items.begin(); k != items.end(); ++k)
-	{
-		if(0 == wcscmp(k->name.c_str(), name))
-		{
-			k->id = ind;
-			k->status = eAdd;
-			return;
-		}
-		type_value p;
-		p.name = name;
-		p.id = ind;
-		p.status = eAdd;
-		items.push_back(p);
-	}
-}
-
-int  CommunicationIDItems::Get(wchar_t *name)
-{
-	for(auto k = items.begin(); k != items.end(); ++k)
-	{
-		if(0 == wcscmp(k->name.c_str(), name))
-		{
-			return k->id;
-		}
-	}
-	return 0;
-}
-
-void CommunicationIDItems::Add(wchar_t *name, int ind)
-{
-	for(auto k = items.begin(); k != items.end(); ++k)
-	{
-		if(0 == wcscmp(k->name.c_str(), name))
-		{
-			k->id = ind;
-			k->status = eAdd;
-			return;
-		}
-	}
-	type_value p;
-	p.name = name;
-	p.id = ind;
-	p.status = eAdd;
-	items.insert(items.begin(), p);
-}
+//void CommunicationIDItems::Add(wchar_t *name, int ind)
+//{
+//	for(auto k = items.begin(); k != items.end(); ++k)
+//	{
+//		if(0 == wcscmp(k->name.c_str(), name))
+//		{
+//			k->id = ind;
+//			k->status = eAdd;
+//			return;
+//		}
+//	}
+//	type_value p;
+//	p.name = name;
+//	p.id = ind;
+//	p.status = eAdd;
+//	items.insert(items.begin(), p);
+//}
 
 void CommunicationIDItems::Clear()
 {
 	items.clear();
-}
-
-unsigned CommunicationIDItems::Count()
-{
-	return items.size();
-}
-
-int CommunicationIDItems::GetID(int ind)
-{
-	if((unsigned)ind < items.size())
-	{
-		return items[ind].id;
-	}
-	return 0;
-}
-wchar_t *CommunicationIDItems::GetName(int ind)
-{
-	if((unsigned)ind < items.size())
-	{
-		return (wchar_t *)items[ind].name.c_str();
-	}
-	return L"";
-}
-
-CommunicationIDItems::Status &CommunicationIDItems::GetStatus(int ind)
-{
-	if((unsigned)ind < items.size())
-	{
-		return items[ind].status;
-	}
-	static Status x;
-	return x;
 }
 
 void CommunicationIDItems::Del(wchar_t *name)
--- AddTresholdWindowProj/SolidGroupAlgoritm/CommunicationIDItems.h
+++ AddTresholdWindowProj/SolidGroupAlgoritm/CommunicationIDItems.h
@@ -4,7 +4,6 @@ class CommunicationIDItems
 {	
 public:
 	enum Status{eOk, eCancel, eAdd};
-	//typedef std::pair<std::wstring, int> type_value;
 	struct type_value
 	{
 	   std::wstring name;
@@ -12,16 +11,9 @@ public:
 	   Status status;
 	};
 	void LoadItem(wchar_t *, int);
-	void Set(wchar_t *, int);
-	int Get(wchar_t *);
 	void Add(wchar_t *, int);
 	void Clear();
-	unsigned Count();
-	int GetID(int);
-	wchar_t *GetName(int);
-	Status &GetStatus(int);
 	void Del(wchar_t *);
 	void Update(wchar_t *pred, wchar_t *current, int id);
-//private:
 	std::vector<type_value> items;
 };
